6db6e4735f044f27420fd350c22904eb5b9558a6
James McDermott <james.mcdermott7@mail.dcu.ie>
2020-04-23 15:21:37 +0000

    FIX: config path creation for python files & yml

--- src/odin-libraries/python/pyodin/odin.py
+++ src/odin-libraries/python/pyodin/odin.py
@@ -1,4 +1,4 @@
-from os import environ, path
+from os import environ, listdir, path
 from sys import exit
 from ruamel.yaml import YAML
 import json
@@ -7,8 +7,9 @@ from time import time
 
 class Odin:
     def __init__(self, config="job.yml", test=False):
-        pathway = path.abspath(".")
-        self.config = pathway + "/" + config
+        for file in listdir("/etc/odin/jobs"):
+            if path.exists("/etc/odin/jobs/" + file + "/" + config):
+                self.config = "/etc/odin/jobs/" + file + "/" + config
         try: 
             with open(self.config,"r") as config:
                 configR = config.read()
